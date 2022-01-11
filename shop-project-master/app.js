79d9feb2f42c294c2169599f79c45e778b1e47e5
Kris Bieliaeva <kris.bieliaeva@gmail.com>
2022-01-11 19:43:34 +0200

    initial commit

---
+++ shop-project-master/app.js
@@ -0,0 +1,14 @@
+const productCountEl = document.getElementById("products-count");
+console.log(productCountEl);
+
+const addtoCartBtns = document.querySelectorAll(".btn-card");
+console.log(addtoCartBtns);
+
+for (let i = 0; i < addtoCartBtns.length; i++) {
+  addtoCartBtns[i].addEventListener("click", function () {
+
+
+    
+  });
+}
+
